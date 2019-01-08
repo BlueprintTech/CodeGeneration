d2d19a6d6b08595c281b067ccdd1a7fb57282fad
Andy Longhurst <andy@longhurst.net>
2019-01-08 12:52:48 +0000

    first transfer of code - doesn't compile

---
+++ CodeGeneration/src/CodeGeneration/Collections.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace Shared.CodeGeneration
+{
+    public static class Collections
+    {
+        public static ImplicitArrayCreationExpressionSyntax AsArraySyntax(this IEnumerable<ExpressionSyntax> expressions)
+        {
+            var withCommas = new List<SyntaxNodeOrToken>();
+            var expressionsAr = expressions.ToArray();
+
+            for (int i = 0; i < expressionsAr.Length; i++)
+            {
+                withCommas.Add(expressionsAr[i]);
+                if (i < expressionsAr.Length - 1)
+                {
+                    withCommas.Add(Tokens.Comma);
+                }
+            }
+
+            return ImplicitArrayCreationExpression(
+                InitializerExpression(
+                    SyntaxKind.ArrayInitializerExpression,
+                    SeparatedList<ExpressionSyntax>(withCommas)));
+        }
+
+        public static ObjectCreationExpressionSyntax InstantiateDictionary(TypeSyntax keyType, TypeSyntax valueType, IEnumerable<ExpressionSyntax> expressions)
+        {
+            var withCommas = new List<SyntaxNodeOrToken>();
+            var expressionsAr = expressions.ToArray();
+
+            for (int i = 0; i < expressionsAr.Length; i++)
+            {
+                withCommas.Add(expressionsAr[i]);
+                if (i < expressionsAr.Length - 1)
+                {
+                    withCommas.Add(Tokens.Comma);
+                }
+            }
+
+            var initializer = InitializerExpression(SyntaxKind.CollectionInitializerExpression, SeparatedList<ExpressionSyntax>(withCommas));
+
+            return ObjectCreationExpression(
+                    GenericName(
+                            Identifier("Dictionary"))
+                        .WithTypeArgumentList(
+                            TypeArgumentList(
+                                SeparatedList<TypeSyntax>(
+                                    new SyntaxNodeOrToken[]
+                                    {
+                                        keyType,
+                                        Tokens.Comma,
+                                        valueType
+                                    }))))
+                .WithInitializer(initializer);
+        }
+
+        public static InitializerExpressionSyntax KeyValuePairInitializer(ExpressionSyntax key, ExpressionSyntax value)
+        {
+            return InitializerExpression(
+                SyntaxKind.ComplexElementInitializerExpression,
+                SeparatedList<ExpressionSyntax>(
+                    new SyntaxNodeOrToken[]
+                    {
+                        key,
+                        Tokens.Comma,
+                        value
+                    }));
+        }
+    }
+}
---
+++ CodeGeneration/src/CodeGeneration/Fields.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace Shared.CodeGeneration
+{
+    public static class Fields
+    {
+        public static FieldDeclarationSyntax PublicConstField(string name, Type type, ExpressionSyntax initialValue)
+        {
+            return Field(name, type, new[] { SyntaxKind.PublicKeyword, SyntaxKind.ConstKeyword }, initialValue);
+        }
+
+        public static FieldDeclarationSyntax Field(string name, Type type, SyntaxKind[] modifiers, ExpressionSyntax initialValue)
+        {
+            var typeDefinition = Types.Type(type);
+
+            return FieldDeclaration(
+                VariableDeclaration(typeDefinition)
+                    .WithVariables(
+                                SingletonSeparatedList<VariableDeclaratorSyntax>(
+                                    VariableDeclarator(
+                                            Identifier(name))
+                                        .WithInitializer(
+                                            EqualsValueClause(initialValue)))))
+                    .WithModifiers(
+                        TokenList(modifiers.AsList())
+            );
+        }
+    }
+}
